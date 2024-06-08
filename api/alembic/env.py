2031138392ab9470b85274f858b2c50abc60ae51
awilliamso6 <awilliamso6@users.noreply.github.com>
2024-06-07 20:06:45 -0400

    Adding docker compose.

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -13,6 +14,9 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+database_url = os.getenv("DATABASE_URL")
+config.set_main_option("sqlalchemy.url", database_url)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
